e15ec04b872df0e6a4b0660d119bae3d5f2bb241
ExoY94 <exoy@mail.de>
2025-03-22 01:26:43 +0100

    Baseline for ProcSetHandler
    + creates a new handler when set is activated
    + destroys the handler when a set is deactivated
    + i can iterate over all active sets
    
    - still happens for all sets

--- ExoYsProcSetTimer.lua
+++ ExoYsProcSetTimer.lua
@@ -7,12 +7,23 @@ local LSD = LibSetDetection
 
 local EPT = ExoYsProcSetTimer 
 
+EPT.acronym = "EPT"
 EPT.name = "ExoYsProcSetTimer"
 EPT.displayName = "ExoY's ProcSet-Timer"
 EPT.version = "3.0.0"
 
 
+--[[ --------------------- ]]
+--[[ -- Update Callback -- ]]
+--[[ --------------------- ]]
 
+local function OnUpdate() 
+    local currentGameTimeMS = GetGameTimeMilliseconds() 
+    for setId, handler in pairs(EPT.ProcSetHandler.list) do 
+        d( zo_strformat("OnUpdate for <<1>> (<<2>>)", LSD.GetSetName(setId), setId))
+        handler:OnTimeUpdate( currentGameTimeMS ) 
+    end
+end
 
 --[[ -------------------- ]]
 --[[ -- Event Callback -- ]]
@@ -29,7 +40,15 @@ end
 
 
 local function OnSetChange( setId, changeType, _, _, activeType ) 
-    
+    LibExoY.Debug("dev", EPT.debug, EPT.acronym, 
+        zo_strformat("SetChange: <<1>> (<<2>>), changeType = <<3>>; activeType = <<4>>", LSD.GetSetName(setId), setId, changeType, activeType)) 
+
+    if changeType == LSD_CHANGE_TYPE_ACTIVATED then 
+        EPT.ProcSetHandler:Create(setId)
+    elseif 
+        changeType == LSD_CHANGE_TYPE_DEACTIVATED then 
+        EPT.ProcSetHandler:Destroy(setId)
+    end 
 end
 
 
@@ -40,6 +59,7 @@ end
 
 local function OnInitialPlayerActivated() 
 
+
 end
 
 
@@ -49,6 +69,12 @@ end
 --[[ -------------------- ]]
 
 local function Initialize() 
+    --- Variable Definition
+    EPT.debug = false 
+    if ExoYsDevelopmentTool then 
+        EPT.debug = ExoYsDevelopmentTool.addonDebug[EPT.name] 
+    end
+
     --- Saved Variables 
 
     
@@ -63,13 +89,18 @@ local function Initialize()
     } 
     LibExoY.CreateSettingsMenu( SettingsMenuParameter ) 
 
+
     --- Event Registration
     LibExoY.RegisterCombatStart( OnCombatStart )
     LibExoY.RegisterCombatEnd( OnCombatEnd ) 
     LibExoY.RegisterForInitialPlayerActivated( OnInitialPlayerActivated )
     LibExoY.RegisterForPlayerActivated( OnPlayerActivated ) 
-
-    LSD:RegisterEvent( LSD_EVENT_SET_CHANGE, EPT.name, OnSetChange, LSD_UNIT_TYPE_PLAYER ) 
+    
+    LSD.RegisterEvent( LSD_EVENT_SET_CHANGE, EPT.name, OnSetChange, LSD_UNIT_TYPE_PLAYER )
+    
+    --- Update Registration 
+    EM:RegisterForUpdate( EPT.name, 5000, OnUpdate )
+    
 end
 
 
