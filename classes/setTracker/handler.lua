20fbcec3cb284be443f28acf50dba8a02c0780a5
ExoY <exoy@mail.de>
2026-05-24 01:52:44 +0200

    add comments

--- classes/setTracker/handler.lua
+++ classes/setTracker/handler.lua
@@ -7,11 +7,14 @@ EPT.setTrackerClass = EPT.setTrackerClass or {}
 EPT.setTrackerClass.handler = SetTrackerHandler 
 
 
+
+
 function SetTrackerHandler:New() 
     local obj = setmetatable({}, self)
     self.__index = self  
 
-    obj.classes = {}
+    obj.classes = {} 
+    obj.specialSets = {}
     obj.objectRegistry = {}    -- list of all already created set tracker objects (no pool) 
     obj.activeObjects = {}    -- list of currently active tracker
 
@@ -21,47 +24,46 @@ end
 
 
 function SetTrackerHandler:ActivateObj( setId )
-    -- creates the object, if this is the first time this set is equipped
+    -- check if set-specific object already exists
+    -- if not, create and initialize it 
     if not self.objectRegistry[setId] then 
-        self:InitializeObj( setId ) -- object is added to registry
+        -- object is automatically added to registry 
+        self:InitializeObj( setId )
     end 
-
     -- grabs object from registry 
     local obj = self.objectRegistry[setId] 
-
-    obj:Activate() 
-
-    table.insert( self.activeObjects, setId )      
-
-    -- chose position 
+    obj:Activate() -- e.g., register events; add fragments to scenes
+    -- keeping track of active objects 
+    table.insert( self.activeObjects, setId )   
 end 
 
 
 
 function SetTrackerHandler:InitializeObj( setId )
-
+    -- *setType* defined in database set "setTracker-class" 
+    -- (main is the superclass, and all classes are initialized on addon loading)
+    -- for any special cases, "specialSets[setId]" defines the unique subclass definition
     local setType = EPT.GetSetType( setId )  
     local obj = self.classes[ setType ]:New( self.specialSets[setId], setId )
     
-    --- class specific initialization
+    -- object specific initialization 
+    -- unique elements for each class/subclass
     obj:Initialize()
+    -- e.g., populate *setData*; create ui-elements; ....
 
-
+    -- add object to registry in handler
     self.objectRegistry[setId] = obj
-    -- get setData 
-    -- populate set data --> there should be a basic function for stuff like setName and a subclass specific function
-    -- check unique 
-    -- create appropriate obj
-    -- get saved variables 
 end
 
 
 
 function SetTrackerHandler:DeactivateObj( setId ) 
+    -- grab obj from registry 
     local obj = self.objectRegistry[setId]
     
+    -- remove setId from numeric list of active sets 
     local objIdx = LibExoY.FindNumericKey( self.activeObjects, setId ) 
     table.remove(self.activeObjects, objIdx )
 
-    obj:Deactivate()
+    obj:Deactivate() --e.g., unregister events, remove fragments from scenes...
 end
